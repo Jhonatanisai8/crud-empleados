bb2dcd5ab0aa72f74d6521004136faa55f1bec86
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-05-02 22:36:22 -0500

    feat: repository y controller

---
+++ backend-app/src/main/java/com/isai/backend_app/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.isai.backend_app.repository;
+
+import com.isai.backend_app.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
